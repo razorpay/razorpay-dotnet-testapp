2b882dba83bdca9041de1916cbb131a673b01f8a
Sunny Aggrawal <sunnyagg@outlook.com>
2016-02-24 00:09:22 +0530

    test app for .net

---
+++ RazorpaySampleApp/Charge.aspx.cs
@@ -0,0 +1,29 @@
+﻿using Razorpay.Api;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace RazorpaySampleApp
+{
+    public partial class Charge : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string paymentId = Request.Form["razorpay_payment_id"];
+
+            Dictionary<string, object> input = new Dictionary<string, object>();
+            input.Add("amount", 100); // this amount should be same as transaction amount
+
+            string key = "Your Key";
+            string secret = "Your Secret";
+
+            RazorpayClient client = new RazorpayClient(key, secret);
+
+            Razorpay.Api.Payment payment = client.Payment.Fetch(paymentId);
+            Razorpay.Api.Payment capturedPayment = payment.Capture(input);
+        }
+    }
+}
---
+++ RazorpaySampleApp/Payment.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Payment.aspx.cs" Inherits="RazorpaySampleApp.Payment" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Razorpay .Net Sample App</title>
+</head>
+<body>
+    <form action="Charge.aspx" method="post">
+<script
+    src="https://checkout.razorpay.com/v1/checkout.js"
+    data-key="<YOUR KEY>"
+    data-amount="100"
+    data-name="Razorpay"
+    data-description="Purchase Description"
+    data-image="https://razorpay.com/favicon.png"
+    data-prefill.name="Harshil Mathur"
+    data-prefill.email="support@razorpay.com"
+    data-theme.color="#F37254"
+></script>
+<input type="hidden" value="Hidden Element" name="hidden">
+</form>
+</body>
+</html>
